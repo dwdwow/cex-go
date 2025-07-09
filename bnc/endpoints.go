c1060c2eaeaf8cae614add6d0744f2db7a853b2c
dwd <wenyi173@gmail.com>
2025-07-09 16:39:31 +0800

    update

---
+++ bnc/endpoints.go
@@ -0,0 +1,14 @@
+package bnc
+
+const (
+	// Spot API endpoints
+	API_ENDPOINT     = "https://api.binance.com"
+	API_ENDPOINT_GCP = "https://api-gcp.binance.com"
+	API_ENDPOINT_1   = "https://api1.binance.com"
+	API_ENDPOINT_2   = "https://api2.binance.com"
+	API_ENDPOINT_3   = "https://api3.binance.com"
+	API_ENDPOINT_4   = "https://api4.binance.com"
+
+	// Spot parent path
+	API_V3 = "/api/v3"
+)
