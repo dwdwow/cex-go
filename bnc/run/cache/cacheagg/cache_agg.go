26a3afceecdc2a72aeae048bcc64155816a06bf3
dwd <wenyi173@gmail.com>
2025-08-24 21:04:39 +0800

    update

---
+++ bnc/run/cache/cacheagg/cache_agg.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/dwdwow/cex-go"
+	"github.com/dwdwow/cex-go/bnc"
+)
+
+func main() {
+	bnc.CacheAggTrades(cex.SYMBOL_TYPE_UM_FUTURES, []string{
+		"BTCUSDT",
+		"ETHUSDT",
+		"SOLUSDT",
+		"1000PEPEUSDT",
+	})
+	bnc.CacheAggTrades(cex.SYMBOL_TYPE_SPOT, []string{
+		"BTCUSDT",
+		"ETHUSDT",
+		"SOLUSDT",
+		"PEPEUSDT",
+	})
+}
--- bnc/ws_cacher.go
+++ bnc/ws_cacher.go
@@ -240,3 +240,113 @@ func CacheAllSymbolsDepthAndBookTicker() {
 	CacheOneTypeAllSymbolsDepthAndBookTicker(cex.SYMBOL_TYPE_SPOT)
 	CacheOneTypeAllSymbolsDepthAndBookTicker(cex.SYMBOL_TYPE_UM_FUTURES)
 }
+
+// spot data use microsecond, futures use millisecond, so should check
+var ms20000101 = time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
+var us20000101 = time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC).UnixMicro()
+
+func CacheOneSymbolAggTrades(symbolType cex.SymbolType, symbol, mongoUri, dbName string, ch, redunCh <-chan PublicStreamMsg[WsAggTradeStream]) {
+	go func() {
+		client, err := mongo.Connect(options.Client().ApplyURI(mongoUri))
+		if err != nil {
+			slog.Error("Can not connect mongo server", "err", err, "uri", mongoUri)
+			panic(err)
+		}
+		db := client.Database(dbName)
+		coll := db.Collection("agg_trades_" + symbol + "_" + string(symbolType))
+		// agg trades are not ordered, so use map to check repetition
+		exist := map[int64]int64{}
+		var msgs []any
+		for {
+			var msg PublicStreamMsg[WsAggTradeStream]
+			select {
+			case msg = <-ch:
+			case msg = <-redunCh:
+			}
+			if msg.Err != nil {
+				slog.Error("bnc: cache agg trades failed", "err", msg.Err)
+				continue
+			}
+			if exist[msg.Stream.AggTradeId] > 0 {
+				continue
+			}
+			exist[msg.Stream.AggTradeId] = msg.Stream.EventTime
+			msgs = append(msgs, msg.Stream)
+			if len(msgs) > 1000 {
+				_, err = coll.InsertMany(context.Background(), msgs)
+				if err != nil {
+					slog.Error("bnc: insert agg trades to mongo failed", "err", err, "agg_trades", msgs)
+					panic(err)
+				}
+				msgs = nil
+			}
+			if len(exist) > 10000 {
+				now := time.Now()
+				newExist := map[int64]int64{}
+				for k, v := range exist {
+					var ti time.Time
+					if v < us20000101 {
+						ti = time.UnixMilli(v)
+					} else {
+						ti = time.UnixMicro(v)
+					}
+					if now.Sub(ti) < time.Second*10 {
+						newExist[k] = v
+					}
+				}
+				exist = newExist
+			}
+		}
+	}()
+}
+
+func CacheAggTrades(symbolType cex.SymbolType, symbols []string) {
+	mongoUri := "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000"
+	dbName := "bnc_realtime_cache"
+
+	wsAggTrade := NewAggTradeWs(nil)
+	redunWsAggTrade := NewAggTradeWs(nil)
+
+	unsubed, err := wsAggTrade.Sub(symbolType, symbols...)
+	if err != nil {
+		fmt.Println(unsubed)
+		panic(err)
+	}
+	aggTradeRedunChs := map[string]chan PublicStreamMsg[WsAggTradeStream]{}
+	for _, symbol := range symbols {
+		aggTradeRedunChs[symbol] = make(chan PublicStreamMsg[WsAggTradeStream], 1000)
+	}
+	go func() {
+		time.Sleep(time.Hour)
+		unsubed, err := redunWsAggTrade.Sub(symbolType, symbols...)
+		if err != nil {
+			fmt.Println(unsubed)
+			panic(err)
+		}
+		for symbol, ch := range aggTradeRedunChs {
+			nc, err := redunWsAggTrade.NewCh(symbolType, symbol)
+			if err != nil {
+				fmt.Println(symbolType, symbol, "redun agg trade", err)
+				panic(err)
+			}
+			go func() {
+				for msg := range nc {
+					ch <- msg
+				}
+			}()
+		}
+	}()
+	for _, symbol := range symbols {
+		ch, err := wsAggTrade.NewCh(symbolType, symbol)
+		if err != nil {
+			fmt.Println(symbolType, symbol, "agg trade", err)
+			panic(err)
+		}
+		redunCh, ok := aggTradeRedunChs[symbol]
+		if !ok {
+			fmt.Println(symbolType, symbol, "redun agg trade not found")
+			panic(fmt.Errorf("redun agg trade not found"))
+		}
+		CacheOneSymbolAggTrades(symbolType, symbol, mongoUri, dbName, ch, redunCh)
+	}
+}
